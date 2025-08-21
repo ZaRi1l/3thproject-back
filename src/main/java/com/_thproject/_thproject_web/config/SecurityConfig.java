8a14869d80438086b646e7389f103d06ca74df53
kimdonghyun296 <2q3w4e5r6t7y@naver.com>
2025-08-21 17:31:58 +0900

    로그인 기능
    로그인 기능

--- src/main/java/com/_thproject/_thproject_web/config/SecurityConfig.java
+++ src/main/java/com/_thproject/_thproject_web/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com._thproject._thproject_web.postgresql.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
