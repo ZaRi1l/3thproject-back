03eb029c427ba734eb8e9a63d9ed9de40fd1bc16
kimdonghyun296 <2q3w4e5r6t7y@naver.com>
2025-08-21 14:29:01 +0900

    Update SwaggerConfig.java

--- src/main/java/com/_thproject/_thproject_web/config/SwaggerConfig.java
+++ src/main/java/com/_thproject/_thproject_web/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com._thproject._thproject_web.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,12 +14,23 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         Info info = new Info()
-                .title("3차 프로젝트 API 문서") // API 문서의 제목
-                .version("1.0.0") // API 버전
-                .description("3차 프로젝트의 백엔드 API 명세서입니다."); // API에 대한 상세한 설명
-        
+                .title("3차 프로젝트 API 문서")
+                .version("1.0.0")
+                .description("3차 프로젝트의 백엔드 API 명세서입니다.");
+
+        // 이 부분이 중요합니다!
+        String jwtSchemeName = "jwtAuth";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
         return new OpenAPI()
-                .components(new Components())
-                .info(info);
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 }
